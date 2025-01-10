eba242dbd0ac07262f9c596a8e2da7fa98012da1
Dinesh L <harshithashivkumar3112@gmail.com>
2025-01-11 00:12:11 +0530

    commit

--- zerone/src/App.jsx
+++ zerone/src/App.jsx
@@ -1,13 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./screens/Home/Home";
 import EventDetails from "./components/EventDetails/EventDetails";
 import "./App.css";
 import { assets } from "./assets/assets";
 
 function App({ onAppMount }) {
-  const [isLoading, setIsLoading] = useState(true);
-
   useEffect(() => {
     // Notify main.jsx to remove loader
     if (onAppMount) onAppMount();
@@ -16,27 +15,17 @@ function App({ onAppMount }) {
   return (
     <Router>
       <div className="App">
-        {/* Loader */}
-        {isLoading && (
-          <div className="loader">
-            <div className="pulse"></div>
-          </div>
-        )}
-
         {/* Background Video */}
-        <video
-          autoPlay
-          muted
-          loop
-          id="background-video"
-          onLoadedData={() => setIsLoading(false)} // Hide loader after video loads
-        >
+        <video autoPlay muted loop id="background-video">
           <source src={assets.bgvideo} type="video/mp4" />
           Your browser does not support the video.
         </video>
 
         {/* Overlay */}
         <div className="overlay"></div>
+
+        {/* Navbar 
+        <Navbar />*/}
 
         {/* Routes */}
         <Routes>
